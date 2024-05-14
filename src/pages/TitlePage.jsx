0fb03b2f63c1851c828a6c550f6de8d69a2957f0
catnofat <catnofat@gmail.com>
2024-05-14 12:57:06 +0900

    feat: titlepage 구현

--- src/pages/TitlePage.jsx
+++ src/pages/TitlePage.jsx
@@ -1,5 +1,34 @@
+import './TitlePage.css'
+import { useNavigate } from 'react-router-dom'
+
 const TitlePage = () => {
-  return <p>TitlePage</p>
+  const navigate = useNavigate()
+  const webclick = () => {
+    navigate('/login')
+  }
+
+  return (
+    <div className="background">
+      <div className="textarea">
+        <div className="smalltext">#음식 #기록</div>
+        <div className="smalltext">#건강 #식단관리</div>
+        <div className="bigtext">EATceed</div>
+      </div>
+
+      <div className="buttonarea">
+        <div
+          className="buttonw"
+          onClick={webclick}>
+          웹버전 로그인
+        </div>
+        <div
+          className="buttonb"
+          onClick={() => window.open('https://play.google.com/')}>
+          어플 다운로드
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default TitlePage
