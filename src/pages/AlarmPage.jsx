eb13d8dbeafb681fd8f5ceb37b236931e51a167f
catnofat <catnofat@gmail.com>
2024-06-03 14:07:27 +0900

    feat:알람기능 추가

--- src/pages/AlarmPage.jsx
+++ src/pages/AlarmPage.jsx
@@ -1,10 +1,20 @@
 import { Fragment } from 'react'
 import SNB from '../components/molecules/SNB'
+import AlarmGroup from '../components/molecules/AlarmGroup'
+import styled from 'styled-components'
+
+const LayoutContainer = styled.div`
+  display: flex;
+  overflow-x: hidden;
+`
 
 const AlarmPage = () => {
   return (
     <Fragment>
-      <SNB cur="alarm" />
+      <LayoutContainer>
+        <SNB cur="alarm" />
+        <AlarmGroup />
+      </LayoutContainer>
     </Fragment>
   )
 }
